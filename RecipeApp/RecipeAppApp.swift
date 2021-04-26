43e32451856eaf499759340c0e040f4337e76c72
samimhakimi <samimgooogl@gmail.com>
2021-04-26 18:35:12 -0400

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Samim Hakimi on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
